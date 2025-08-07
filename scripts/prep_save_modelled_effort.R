e5c4aed491ac13ad009031f61cb4ecc6b888c536
fishingeffort <fishingeffort@data.novalocal>
2025-08-07 22:38:53 +0000

    testing artisanal data

--- scripts/prep_save_modelled_effort.R
+++ scripts/prep_save_modelled_effort.R
@@ -22,7 +22,32 @@ for(file in all_files){
   
   file_name <- str_replace(basename(file), "\\.csv", "\\.qs")
   
-  qs::qsave(data, here(glue("rf_model_data/{file_name}")))
+  qs::qsave(data, here(glue("rf_model_data_ind/{file_name}")))
+  
+  
+}
+
+test <- qs::qread(here("rf_model_data_ind/model_preds_1950_2017_AGO.qs"))
+
+
+data_dir <- file.path("/homevol/fishingeffort/data_storage")
+
+all_files <- list.files(file.path(data_dir, "prep/random_forest/zenodo_data/artisanal_skylight_sentinel/mapped_by_flag_country/"), full.names = TRUE)
+
+for(file in all_files){
+  
+  #  file <- all_files[118]
+  
+  data <- read.csv(file) %>%
+    dplyr::select(-data_type, -pixel_area_m2, -nom_hours_km2, -nom_days_km2, -eff_hours_km2, -eff_days_km2, -pixel_id)
+  
+  if(nrow(data) == 0){
+    next()
+  }
+  
+  file_name <- str_replace(basename(file), "\\.csv", "\\.qs")
+  
+  qs::qsave(data, here(glue("rf_model_data_art/{file_name}")))
   
   
 }
--- scripts/prep_timeseries_plot_data.R
+++ scripts/prep_timeseries_plot_data.R
@@ -7,7 +7,7 @@ library(here)
 
 data_dir <- file.path("/homevol/fishingeffort/data_storage")
 
-all_files <- list.files(here("rf_model_data/"), full.names = TRUE)
+all_files <- list.files(here("rf_model_data_ind/"), full.names = TRUE)
 
 all_data <- lapply(all_files, qs::qread) %>%
   bind_rows()
@@ -21,7 +21,27 @@ all_data_tidy <- all_data %>%
   mutate(sector = "Industrial")
 
   
-  qs::qsave(all_data_tidy, here("timeseries_data/all_timeseries_data_grouped.qs"))
+  qs::qsave(all_data_tidy, here("timeseries_data/all_timeseries_data_grouped_ind.qs"))
   
   
 
+  ### read in all prediction data and save to rf_model_data folder
+  
+  all_files <- list.files(here("rf_model_data_art/"), full.names = TRUE)
+  
+  all_data <- lapply(all_files, qs::qread) %>%
+    bind_rows()
+  
+  all_data_tidy <- all_data %>%
+    group_by(year, flag_country_iso3c, flag_country_name, eez_sovereign_iso3c, 
+             eez_sovereign_name, fao_fishing_id, fao_major_fishing_area, length_category) %>%
+    summarise(nom_active_fishing_days = sum(nom_active_fishing_days, na.rm = TRUE),
+              eff_active_fishing_days = sum(eff_active_fishing_days, na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(sector = "Artisanal")
+  
+  
+  qs::qsave(all_data_tidy, here("timeseries_data/all_timeseries_data_grouped_art.qs"))
+  
+  
+  
